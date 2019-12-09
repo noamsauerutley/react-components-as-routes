15c1be362c5fb2313271b6be0dbe88285d971ec0
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-12-09 17:17:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -8,8 +9,37 @@ const Home = () => {
     </div>
   );
 };
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
+</Router>),
   document.getElementById('root')
 );
